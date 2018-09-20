75bb7bf68fec1de1a4916f00a575c72afb6b11bb
fanan <fanan.dala@yahoo.com>
2018-09-20 15:31:29 +0100

    Added HasRoles Trait to User Model

--- app/Models/user.php
+++ app/Models/user.php
@@ -5,9 +5,10 @@ namespace billiard\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class user extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable,HasRoles;
     //
 }
